d4415c5f2c86f2adfb7d397f7d613de76b3c5a09
vaibhav <vaibhavacharya305@gmail.com>
2026-09-05 01:58:53 +0530

    Bypass build-time env var checks for Cloudflare

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -3,4 +3,7 @@ import config from '../../../../../keystatic.config';
 
 export const { GET, POST } = makeRouteHandler({
   config,
+  clientId: process.env.KEYSTATIC_GITHUB_APP_CLIENT_ID || 'dummy_id_to_bypass_build_check',
+  clientSecret: process.env.KEYSTATIC_GITHUB_APP_CLIENT_SECRET || 'dummy_secret_to_bypass_build_check',
+  secret: process.env.KEYSTATIC_SECRET || 'dummy_secret_to_bypass_build_check',
 });
