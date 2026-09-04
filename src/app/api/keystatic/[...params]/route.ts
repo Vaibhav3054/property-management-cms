ba8efc05a0baf63161a73c3d7d84d226b1949af7
vaibhav <vaibhavacharya305@gmail.com>
2026-09-05 02:29:26 +0530

    Add edge runtime and explicit env vars

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { makeRouteHandler } from '@keystatic/next/route-handler';
 import config from '../../../../../keystatic.config';
 
@@ -5,7 +6,12 @@ let handler: any;
 
 function getHandler() {
   if (!handler) {
-    handler = makeRouteHandler({ config });
+    handler = makeRouteHandler({ 
+      config,
+      clientId: process.env.KEYSTATIC_GITHUB_APP_CLIENT_ID,
+      clientSecret: process.env.KEYSTATIC_GITHUB_APP_CLIENT_SECRET,
+      secret: process.env.KEYSTATIC_SECRET,
+    });
   }
   return handler;
 }
